2353a8a8549e4415e65a56edc817d1cec9c9c017
Ziad Alame <ziadalame@gmail.com>
2017-08-04 05:06:43 +0300

    fix: handled click on none to not do anything

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,7 +11,7 @@ function Book({ book, changeShelf }) {
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                        <select value={book.shelf || 'none'} onChange={(changeEvent) => (changeShelf(id, changeEvent.target.value))}>
+                        <select value={book.shelf || 'none'} onChange={(changeEvent) => (changeEvent.target.value !== 'none' ? changeShelf(id, changeEvent.target.value) : null )}>
                             <option value="none" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
