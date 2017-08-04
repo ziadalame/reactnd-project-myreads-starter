079d30e3bc26004540a19933fc486b8e4ee6d0d5
Ziad Alame <ziadalame@gmail.com>
2017-08-04 05:03:22 +0300

    fix: added comments and changed the type of ListBooks component

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react'
-
+import React from 'react'
+// Link to rout to pages
 import { Link } from 'react-router-dom'
-
+// for manipulation of quick utiity function
 import _ from 'lodash'
-
+// Prop-type for vaidation
 import PropTypes from 'prop-types'
-
+// shelf component
 import Shelf from './Shelf';
 
-import '../App.css'
+function ListBooks(props) {
 
-class ListBooks extends Component {
-
-    render() {
-
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        {Object.keys(this.props.shelves).map((title) => {
-                            return (<Shelf
-                                key={title}
-                                title={_.startCase(title)}
-                                books={this.props.shelves[title]}
-                                changeShelf={this.props.changeShelf}
-                            />)
-                        })}
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link to='/search'>Add a book</Link>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    {Object.keys(props.shelves).map((title) => {
+                        return (<Shelf
+                            key={title}
+                            title={_.startCase(title)}
+                            books={props.shelves[title]}
+                            changeShelf={props.changeShelf}
+                        />)
+                    })}
                 </div>
             </div>
-        )
-    }
+            <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+            </div>
+        </div>
+    )
+
 }
 
 ListBooks.propTypes = {
