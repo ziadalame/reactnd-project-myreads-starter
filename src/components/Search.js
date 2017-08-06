399e71dd825ad68112013d24c2a23967fa14c224
Ziad Alame <ziadalame@gmail.com>
2017-08-06 06:48:56 +0300

    Fix: fixed order of messages in search

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,8 +24,8 @@ class Search extends Component {
                     </div>
                 </div>
                 <div className="search-books-results">
-                    {this.props.error && (<div className="search-error">{this.props.error}</div>)}
                     {this.props.queryString && (<div className="search-query" >Showing results for: {this.props.queryString}</div>)}
+                    {this.props.error && (<div className="search-error">{this.props.error}</div>)}
                     <ol className="books-grid">
                         {this.props.books.map((book) => {
                             return (
